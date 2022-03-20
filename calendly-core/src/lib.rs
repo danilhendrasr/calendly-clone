1b61bb2fd724f6369a307d85fa816e7ebbf73bef
Danil Hendra Suryawan <danilhendrasr@gmail.com>
2022-03-20 20:25:49 +0700

    more code

--- calendly-core/src/lib.rs
+++ calendly-core/src/lib.rs
@@ -1,15 +1,34 @@
 use std::time;
 
-struct TimeSlot {
-    
+pub struct Event {
+    pub name: String,
+    pub description: String,
+    pub duration: time::Duration,
+    pub event_link: String,
+
+    // Date string, ex: "2021-02-17"
+    pub date_start: String,
+    // Date string, ex: "2022-02-17", if null, the end is indefinite
+    pub date_end: Option<String>,
+
+    pub time_start: u32,
+    pub time_end: u32,
 }
 
-struct AvailableDate {
-
+pub struct Month {
+    pub year: u32,
+    pub month: u16,
 }
 
-struct Event {
-    name: String,
-    duration: time::Duration,
-    available_dates: Vec<AvailableDate>
+pub struct DateSlot {
+    date: u32,
+    time_slots: Vec<String>,
+}
+
+impl Event {
+    pub fn get_month_schedule(&self, month: &Month) -> Vec<DateSlot> {
+        // Jika self.date_start dan self.date_end tidak berada di bulan yang dipilih
+        // maka return semua tanggal yang available pada bulan tersebut.
+        Vec::new()
+    }
 }
